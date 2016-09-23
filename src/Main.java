0331a05ecbcedaebd0dc424e1bccac809d7faaf9
Bill Yu <hy103@duke.edu>
2016-09-23 19:33:42 -0400

    remove animation from Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 
 
 import global.Initializer;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import ui.StartScene;
 
 /**
@@ -17,13 +14,7 @@ import ui.StartScene;
  */
 
 public class Main extends Application {
-    private static final int FRAMES_PER_SECOND = 60;
-    private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
-    private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-
     private Stage stage;
-    private KeyFrame frame;
-    private Timeline animation;
     
     private Initializer initializer;
     
@@ -42,25 +33,13 @@ public class Main extends Application {
     	this.stage = s;
         stage.setTitle("Cell Society");
         stage.show();
-
-        // sets the program loop
-        KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY),
-                                      e -> this.step(SECOND_DELAY));
-        Timeline animation = new Timeline();
-        animation.setCycleCount(Timeline.INDEFINITE);
-        animation.getKeyFrames().add(frame);
-        animation.play();
         
         initializer = new Initializer(stage);
         
         StartScene start = new StartScene(new ExitAction(), initializer);
         stage.setScene(start.initScene());
     }
-
-    public void step (double elapsedTime) {
-    	
-    }
-
+    
 	/**
      * Start the program.
      */
