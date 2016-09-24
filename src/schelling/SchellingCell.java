8988477ed77eba7f4ebc7ad4502a41466ed8e328
Addison Howenstine <arh55@duke.edu>
2016-09-24 15:07:47 -0400

    Debugging Schelling. Still currently creates a duplicate when unsatisfied cell moves

--- src/schelling/SchellingCell.java
+++ src/schelling/SchellingCell.java
@@ -1,5 +1,8 @@
 package schelling;
 
+import java.util.Collections;
+import java.util.List;
+
 import javafx.scene.paint.Color;
 import cell.Cell;
 import cell.GridPosition;
@@ -7,36 +10,49 @@ import cell.State;
 
 public class SchellingCell extends Cell{
 
-	private double idealRatio; // between 0 and 1
+	private double idealRatio = 0.5; // between 0 and 1
 	public static State personX = new State(Color.RED, "X");
 	public static State personO = new State(Color.BLUE, "O");
 	public static State vacant = new State(Color.GRAY, "VACANT");
+	private List<Cell> cells;
 
-	
+
 	public SchellingCell(GridPosition gridPosition, State s) {
 		super(gridPosition, s);
 	}
 
 	@Override
 	public void checkChangeState() {
-		if(!checkSatisfied())
+		if(!this.getCurrState().equals(vacant) && !checkSatisfied()){
 			moveToEmptyCell();
+		}
 	}
 
 	private boolean checkSatisfied(){
 		int numSimilar = 0;
-		for(Cell neighbor : getNeighbors())
+		int numDifferent = 0;
+		for(Cell neighbor : getNeighbors()){
 			if (neighbor.getCurrState().equals(this.getCurrState()))
 				numSimilar++;
-		return ( (double) numSimilar / (double) getNeighbors().size() > idealRatio);
+			if (!(neighbor.getCurrState().equals(vacant) || neighbor.getCurrState().equals(this.getCurrState())))
+				numDifferent++;
+		}
+		int numNeighbors = numSimilar + numDifferent;
+		return ( numNeighbors == 0 || ((double) numSimilar / (double) (numNeighbors)) > idealRatio);
 	}
 
 	private void moveToEmptyCell(){
-		// TODO replace getNeighbors with a way to loop through all cells, not just neighbors
-		for(Cell option : getNeighbors())
+		List<Cell> toShuffle = cells;
+		Collections.shuffle(toShuffle);
+		for(Cell option : toShuffle)
 			if(option.getFutureState().equals(vacant)){
 				option.setFutureState(this.getCurrState());
 				this.setFutureState(vacant);
+				return;
 			}
+	}
+
+	public void setCellsPointer(List<Cell> cells){
+		this.cells = cells;
 	}
 }
