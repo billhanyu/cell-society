ef0c4290caf145cb47b5523bc518c2bad3447c96
Brian Keohane <bk112@duke.edu>
2016-09-25 21:32:21 -0400

    Updated GameOfLife to work from XML given specific conditions

--- src/cell/GridPosition.java
+++ src/cell/GridPosition.java
@@ -5,6 +5,10 @@ public class GridPosition {
 	private int gridRow;
 	private int gridCol;
 	
+	public GridPosition(GridPosition p){
+	    this.gridRow = p.getRow();
+	    this.gridCol = p.getCol();
+	}
 	public GridPosition(int r, int c){
 		gridRow = r;
 		gridCol = c;
@@ -20,6 +24,15 @@ public class GridPosition {
 			return false;
 		GridPosition other = (GridPosition) obj;
 		return (gridCol == other.gridCol && gridRow == other.gridRow);
+	}
+	
+	@Override
+	public int hashCode(){
+            int prime = 31;
+            int hashCode = 1;
+            hashCode = prime * hashCode + gridRow;
+            hashCode = prime * hashCode + gridCol;
+	    return hashCode;
 	}
 	
 	public int getRow(){
---
+++ src/xml/GameOfLifeSimulationFactory.java
@@ -0,0 +1,50 @@
+package xml;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import cell.GridPosition;
+import gameOfLife.GLParameters;
+import grid.Parameters;
+import schelling.SLParameters;
+
+public class GameOfLifeSimulationFactory extends SimulationFactory {
+    
+    private List<String> listOfAlive;
+    
+    public GameOfLifeSimulationFactory(Element rootElement) {
+        super(rootElement);
+    }
+
+    @Override
+    public String getSimulationType () {
+        return "GameOfLife";
+    }
+
+    @Override
+    public GLParameters getSimulationParameters () {
+        
+        listOfAlive = new ArrayList<String>();
+        Parameters basicParams = returnBasicParameters();
+        //NodeList listTagNames = rootElement.getElementsByTagName("*");
+        NodeList hello = rootElement.getChildNodes();
+        for (int i = 0; i < hello.getLength(); i++){
+            if (hello.item(i).getNodeName().equals("Alive")){
+                String alivePos = new String(hello.item(i).getTextContent());
+                listOfAlive.add(alivePos);
+            }
+        }
+        if (listOfAlive.size() > 1){
+            return new GLParameters(basicParams, listOfAlive);
+        }
+        else{
+            //String aliveRatio = getTextValue("aliveRatio");
+            return new GLParameters(basicParams, ".4");
+        }
+        
+    }
+
+}
+
+
