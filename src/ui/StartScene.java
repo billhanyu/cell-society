7f6065f70ff6fc3d007d852090993448d39fafb3
Bill Yu <hy103@duke.edu>
2016-10-01 12:37:14 -0400

    change startscene strings to resource

--- src/ui/StartScene.java
+++ src/ui/StartScene.java
@@ -1,26 +1,20 @@
 package ui;
 
 import java.util.ResourceBundle;
+
 import global.Initializer;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 public class StartScene extends ProgScene {
-
-	private ComboBox<String> comboBox;
-	private String algorithm;
-	private String goString = "Go";
+	
 	private EventHandler<ActionEvent> exitAction;
 	private Initializer initializer;
 	private ResourceBundle myResource;
@@ -30,7 +24,6 @@ public class StartScene extends ProgScene {
 		this.exitAction = exitAction;
 		this.initializer = initializer;
 	}
-
 
 	public Scene initScene(int size, ResourceBundle myResource) {
 		this.myResource = myResource;
@@ -46,58 +39,8 @@ public class StartScene extends ProgScene {
 		return new Scene(root, width, height);
 	}
 
-//	private ComboBox<String> initComboBox() {
-//		ObservableList<String> options = 
-//				FXCollections.observableArrayList(
-//						Initializer.SEGREGATION,
-//						Initializer.PRED_PREY,
-//						Initializer.FIRE,
-//						Initializer.LIFE
-//						);
-//		comboBox = new ComboBox<String>(options);
-//		comboBox.setPromptText("Select a Simulation");
-//		comboBox.setPrefWidth(200);
-//		comboBox.setOnAction((e) -> {
-//			algorithm =  comboBox.getSelectionModel().getSelectedItem().toString();    
-//		});
-//		return comboBox;
-//	}
-
-//	private VBox initCombinedSelectionBox(){
-//		VBox box = new VBox(10);
-//		HBox selectionBox = initSelectionBox();
-//		Button chooseFile = initFileButton();
-//		box.getChildren().addAll(selectionBox, chooseFile);
-//		box.setAlignment(Pos.CENTER);
-//		return box;
-//
-//	}
-
-//	//drop down and go button
-//	private HBox initSelectionBox() {
-//		ComboBox<String> selection = initComboBox();
-//		Button goButton = initGoButton();
-//		HBox box = new HBox();
-//		box.getChildren().addAll(selection, goButton);
-//		box.setAlignment(Pos.CENTER);
-//		return box;
-//	}
-
-//	private Button initGoButton() {
-//		Button go = new Button(goString);
-//		go.setOnAction(e->{
-//			if (algorithm == null) {
-//				ErrorPop pop = new ErrorPop(300, 200, myResource.getString("GoError"), myResource);
-//				pop.popup();
-//				return;
-//			}
-//			initializer.initSimulation(algorithm);
-//		});
-//		return go;
-//	}
-
 	private Button initFileButton() {
-		Button chooseFile = new Button("Choose File");
+		Button chooseFile = new Button(this.myResource.getString("ChooseFile"));
 		chooseFile.setOnAction(e ->{
 			initializer.initSimulationFromFile();
 		});
@@ -105,13 +48,13 @@ public class StartScene extends ProgScene {
 	}
 
 	private Label initTitle() {
-		Label lbl = new Label("Cell Society");
+		Label lbl = new Label(this.myResource.getString("Title"));
 		lbl.setFont(new Font(20));
 		return lbl;
 	}
 
 	private Button initExitButton() {
-		Button exit = new Button("Exit");
+		Button exit = new Button(this.myResource.getString("Exit"));
 		exit.setPrefWidth(100);
 		exit.setOnAction(exitAction);
 		return exit;
